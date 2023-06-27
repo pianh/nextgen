18888b30ba6f7786da4794f6987f677957c7eadd
Duy Anh <ndanhdev@gmail.com>
2023-06-27 12:41:30 +0700

    setup

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/header/HeaderDefault';
+import Footer from '../components/footer/FooterDefault';
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <div className='test'>Home âsssssa</div>
+    <Header/>
+      <div className='wrapper_home'></div>
+      {/* <Footer/> */}
     </>
   )
 }
